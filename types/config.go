9e490ae8ffcbd212f89708e730cd1909d5aa0781
chrobles <chrobles@microsoft.com>
2020-03-19 10:38:08 -0400

    +dockerfile and refactors for resolving datapath in container

--- types/config.go
+++ types/config.go
@@ -2,16 +2,16 @@ package types
 
 // Config : captures app config from env
 type Config struct {
+	Root          string
 	CosmosDB      CosmosDbConfig
 	CoinMarketCap CoinMarketCapConfig
 }
 
 // CoinMarketCapConfig : config for cmc api client
 type CoinMarketCapConfig struct {
-	BaseURL       string `envconfig:"CMC_API_BASEURL"`
-	Key           string `envconfig:"CMC_API_KEY"`
-	LocalDataPath string `envconfig:"CMC_LOCAL_DATA_PATH"`
-	UseLocal      bool   `envconfig:"CMC_USE_LOCAL"`
+	BaseURL  string `envconfig:"CMC_API_BASEURL"`
+	Key      string `envconfig:"CMC_API_KEY"`
+	UseLocal bool   `envconfig:"CMC_USE_LOCAL"`
 }
 
 // CosmosDbConfig : config for cosmosdb client
